42727db6eac2157c5ad6db72abe98175661768e9
JhowEight <jhowzinhobr15@gmail.com>
2025-02-05 21:56:06 -0300

    Pegando valor escrito no input e salvando

---
+++ app/cadastro/page.js
@@ -0,0 +1,32 @@
+'use client'
+
+import { useState } from "react";
+
+
+function Cadastro() {
+
+    const [nome, setNome] = useState("");
+
+    function salvar(){
+        console.log("O nome cadastrado é:"+nome);
+    }
+
+    return ( 
+        <div className="p-10" >
+
+            <h1 className="text-lg" >Cadastro</h1>
+            
+            <hr/>
+
+        <p>Digite seu nome:</p>
+        <input onChange={((e)=> setNome( e.target.value ) )} className="outline" />
+
+        <br/>
+
+        <button  onClick={()=> salvar()} value={nome} className="p-3 mt-5 bg-lime-500 text-white ">Salvar</button>
+
+        </div>
+     );
+}
+
+export default Cadastro;
